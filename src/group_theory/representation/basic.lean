204473f67e6e5cfb972c477ae1f6d690fe57524c
lean2 <lean2@pop-os.localdomain>
2020-04-20 11:44:30 -0400

    add invariant subspace stuff

--- src/group_theory/representation/basic.lean
+++ src/group_theory/representation/basic.lean
@@ -2,14 +2,13 @@
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: ...
 -/
-import linear_algebra.basic linear_algebra.finite_dimensional
-import algebra.module
+import linear_algebra.basic linear_algebra.finite_dimensional linear_algebra.bilinear_form
+import algebra.module logic.unique
 
 universe variables u v w w'
 
 open linear_map
 
-/- maybe needs a shorter name -/
 /-- A representation of a group `G` on an `R`-module `M` is a group homomorphism from `G` to
   `GL(M)`. Normally `M` is a vector space, but we don't need that for the definition. -/
 def group_representation (G R M : Type*) [group G] [ring R] [add_comm_group M] [module R M] :
@@ -22,6 +21,24 @@ variables {G : Type u} {R : Type v} {M : Type w} {M' : Type w'}
 instance : has_coe_to_fun (group_representation G R M) := ⟨_, λ f, f.to_fun⟩
 
 namespace group_representation
+
+/- module facts -/
+
+def complementary [module R M] (N N' : submodule R M) : Prop := 
+  ∀ x : M, ∃! (n : N), ∃! (n' : N'), x = n + n' 
+
+def is_projection (π: M →ₗ[R] M) : Prop := π ∘ π = π 
+
+example (π : M →ₗ[R] M) : is_projection π → complementary (ker π) (range π) := 
+begin unfold is_projection, intro, unfold ker, unfold range, --unfold complementary, 
+end
+
+def projector_on_submodule [module R M] (N : submodule R M) : M →ₗ[R] M := sorry 
+
+def is_orthogonal_complement (B : bilin_form R M) (N N' : submodule R M) : Prop :=
+  complementary N N' ∧ ∀ x : N, ∀ y : N', bilin_form.is_ortho B x y 
+
+/- end module facts -/
 
 /- do we want this instance? Then we don't have to write `(ρ g).1 x` instead of `ρ g x`. -/
 instance : has_coe (general_linear_group R M) (M →ₗ[R] M) := ⟨λ x, x.1⟩
@@ -39,6 +56,14 @@ structure subrepresentation (ρ : group_representation G R M) (π : group_repres
 def invariant_subspace (ρ : group_representation G R M) (N : submodule R M) : Prop :=
   ∀ x : N, ∀ g : G, ρ g x ∈ N
 
+variables B : bilin_form R M 
+
+def standard_invariant_bilinear_form (ρ : group_representation G R M) (B : bilin_form R M): bilin_form R M := B 
+  -- linear_map.to_bilin  λ (x y : M), sum over g in  G, B (ρ g x) (ρ g y)
+  -- also divide by |G|
+
+#print standard_invariant_bilinear_form
+
 /- this requires the cokernel of α
 lemma subrep_is_invariant (ρ : group_representation G R M) (π : group_representation G R M') :
   Π s : subrepresentation ρ π, invariant_subspace ρ (s.α M) :=
@@ -47,13 +72,28 @@ lemma subrep_is_invariant (ρ : group_representation G R M) (π : group_represen
 def irreducible (ρ : group_representation G R M) : Prop :=
   ∀ N : submodule R M, invariant_subspace ρ N → N = ⊥ ∨ N = ⊤
 
--- presumably this exists somewhere ?
-def complementary [module R M] (N N' : submodule R M) : Prop := sorry
 
 /-- Maschke's theorem -/
 
-theorem maschke (ρ : group_representation G R M) : ∀ N : submodule R M,
-  invariant_subspace ρ N → ∃ N', invariant_subspace ρ N' ∧ complementary N N' := sorry
+lemma standard_orthogonal_complement_is_invariant (ρ : group_representation G R M) (B : bilin_form R M) : 
+  ∀ N N' : submodule R M, invariant_subspace ρ N → is_orthogonal_complement B N N' → invariant_subspace ρ N' := sorry
+
+theorem maschke (ρ : group_representation G R M) (B : bilin_form R M) : ∀ N : submodule R M,
+  invariant_subspace ρ N → ∃ N', invariant_subspace ρ N' ∧ complementary N N' := 
+begin intros, have h := standard_orthogonal_complement_is_invariant ρ B , 
+
+end
+
+def Mmap := M →ₗ[R] M 
+
+def invariant_projector (ρ : group_representation G R M) (B : bilin_form R M) : Mmap → Mmap := λ X, X 
+-- λ x : M, sum over g, ρ g (π (ρ g⁻¹ x))
+
+theorem maschke2 (ρ : group_representation G R M) (B : bilin_form R M) : ∀ N : submodule R M,
+  invariant_subspace ρ N → ∃ N', invariant_subspace ρ N' ∧ complementary N N' := 
+begin intros, 
+  use invariant_projector ρ B projector_on_submodule N, 
+end
 
 end group_representation
 
